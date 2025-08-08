07903aec96ff099e14bfb43bc5a4b9c26fe60793
nburns1 <nicole_burns@brown.edu>
2025-08-08 16:39:49 -0400

    testing

--- dataprepv2.R
+++ dataprepv2.R
@@ -198,7 +198,6 @@ wonder_data_imputed <- wonder_data_with_pop %>%
   )
 wonder_data_imputed <- wonder_data_imputed %>%
   select(GEOID, County, Deaths, Deaths_imputed)
-
 #######################
 #XIS 
 library(arrow)
